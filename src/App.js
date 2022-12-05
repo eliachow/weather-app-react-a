1435b250cd70458feba84a2ef196265f36810daf
eliachow <eliacchow@gmail.com>
2022-12-04 21:31:20 -0800

    Created header & search bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="header">Weather Test</h1>
+      <div className="top-section">
+        <div className="header">Weather App</div>
+        <form class="row search-bar">
+          <div class="col-6">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter a city"
+            />
+          </div>
+          <div class="col-3">
+            <input type="submit" class="form-control" value="Search" />
+          </div>
+          <div class="col-3">
+            <input type="submit" class="form-control" value="Location" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
